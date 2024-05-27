f22d7f70cb3101f276f22cc08919c5fada47f4cc
SanamKhudoyberdieva <khudoyberdievasanam@gmail.com>
2024-05-27 14:32:20 +0500

    index page

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(".re-partners-slider").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow:
+      '<button class="slick-prev slick-arrow slick-white-arrow" aria-label="Previous" type="button"><img src="./img/ic_slick_left.svg"/></button>',
+    nextArrow:
+      '<button class="slick-next slick-arrow slick-white-arrow" aria-label="Next" type="button"><img src="./img/ic_slick_right.svg"/></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
